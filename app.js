239d37d3deac0b4599ff9630d8c0a3b1c4f8f621
Emmanuel Wave <jonahemma12@gmail.com>
2021-09-15 23:00:21 +0100

    in  Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Hello World');
+
+
+// How to comment inline
+
+//alert('This is my work');
+
+// Variables
+
+var b = 'Fortune';
+console.log(b);
+
+var someNumber = 144;
+console.log(someNumber);
+
+
